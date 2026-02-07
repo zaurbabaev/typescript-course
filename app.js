3f7f888f842ef9ea111ddc0145ff83775b25a3de
Zaur <zaurik87@gmail.com>
2026-02-07 12:03:57 +0400

    data types

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-console.log("Hello TypeScript");
+/* console.log("Hello TypeScript");
 console.log("TyprScript kodlama davam ediyor");
 console.log("Kodlama davam ediyor");
+
+ */
+function add(n1, n2) {
+    return n1 + n2;
+}
+var number1 = 4;
+var number2 = 5.2;
+var result = add(number1, number2);
+console.log(result);
--- app.ts
+++ app.ts
@@ -1,3 +1,16 @@
-console.log("Hello TypeScript");
+/* console.log("Hello TypeScript");
 console.log("TyprScript kodlama davam ediyor");
 console.log("Kodlama davam ediyor");
+
+ */
+
+function add(n1: number, n2: number): number {
+  return n1 + n2;
+}
+
+const number1: number = 4;
+const number2: number = 5.2;
+
+const result: number = add(number1, number2);
+
+console.log(result);
